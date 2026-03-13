b5a5caa32307c168af3dbb2c2e35c9648f92d74e
VictorKornilov203 <166919357+VictorKornilov203@users.noreply.github.com>
2026-03-14 01:12:58 +0700

    Update delete_pos.php

--- admin/delete_pos.php
+++ admin/delete_pos.php
@@ -9,21 +9,18 @@ if (!isAdmin()) {
 $post_id = isset($_GET['id']) ? (int)$_GET['id'] : 0;
 
 if ($post_id) {
-    // Сначала получаем информацию о посте, чтобы удалить картинку
+    
     $stmt = $pdo->prepare("SELECT image FROM posts WHERE id = ?");
     $stmt->execute([$post_id]);
     $post = $stmt->fetch();
     
-    // Удаляем картинку, если она есть
+   
     if ($post && $post['image'] && file_exists('../' . $post['image'])) {
         unlink('../' . $post['image']);
     }
     
-    // Удаляем пост (комментарии удалятся автоматически из-за FOREIGN KEY)
     $stmt = $pdo->prepare("DELETE FROM posts WHERE id = ?");
     $stmt->execute([$post_id]);
 }
-
-// Возвращаемся на страницу с постами
 redirect('posts.php');
 ?>
